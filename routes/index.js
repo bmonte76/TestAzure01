3018a4f00be1df3dc39cb4092c4064b686e57b50
Brian Montef <Brian Montef>
2016-07-30 16:07:32 -0600

    add Bola

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hola Cara De BOLA!!!' });
 });
 
 module.exports = router;
